cc2c9259552f4286fc48b728d2c417df58bd35ad
Dmytro Shpatakovskyi <distefano.nonoficial@gmail.com>
2026-05-03 16:07:59 +0200

    changed docker-compose.yaml & config.py, add .env.example

--- INT26-24/src/app/config.py
+++ INT26-24/src/app/config.py
@@ -10,7 +10,7 @@ class Settings(BaseSettings):
     collection_name: str
 
     model_config = SettingsConfigDict(
-        env_file=".env",
+        env_file=".env.example",
         env_file_encoding="utf-8"
     )
 
