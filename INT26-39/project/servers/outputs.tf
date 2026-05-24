75c32d8dcc2cdb407fdef2cc8a8921d8f567008a
Dmytro Shpatakovskyi <distefano.nonoficial@gmail.com>
2026-05-24 15:30:51 +0200

    added output ssh_command

--- INT26-39/project/servers/outputs.tf
+++ INT26-39/project/servers/outputs.tf
@@ -1,7 +1,12 @@
-output "web_ec2" {
+output "ssh_command" {
   value = "ssh -i ${path.module}/keys/id_rsa ubuntu@${aws_instance.web.public_ip}"
+}
+
+output "web_public_ip" {
+  value = aws_instance.web.public_ip
 }
 
 output "private_ip_mongodb" {
   value = aws_instance.mongodb.private_ip
 }
+
